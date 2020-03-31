b7cf63e8313b3826cb8f96457d19e68c9c338235
罗国雄 <luoguoxiong@ipin.com>
2020-03-31 11:12:08 +0800

    add mysql toml

--- common/goods/internal/dao/mysql.go
+++ common/goods/internal/dao/mysql.go
@@ -1,14 +1,35 @@
 package dao
 
 import (
+	"github.com/bilibili/kratos/pkg/conf/paladin"
 	_ "github.com/go-sql-driver/mysql" // init
 	"github.com/jinzhu/gorm"
 )
 
+type cfg struct {
+	Dsn          string
+	Debug        bool
+	MaxOpenConns int
+	MaxIdleConns int
+}
+
 // NewDB ...
 func NewDB() (db *gorm.DB, cf func(), err error) {
-	db, err = gorm.Open("mysql", "root:ectripaes2013@tcp(202.96.155.121)/nideshop?charset=utf8&parseTime=True&loc=Local")
-	db.LogMode(true)
+
+	var (
+		conf cfg
+		ct   paladin.TOML
+	)
+	if err = paladin.Get("mysql.toml").Unmarshal(&ct); err != nil {
+		return
+	}
+	if err = ct.Get("GoodsDB").UnmarshalTOML(&conf); err != nil {
+		return
+	}
+	db, err = gorm.Open("mysql", conf.Dsn)
+	db.DB().SetMaxIdleConns(conf.MaxIdleConns)
+	db.DB().SetMaxOpenConns(conf.MaxOpenConns)
+	db.LogMode(conf.Debug)
 	cf = func() { db.Close() }
 	return
 }
